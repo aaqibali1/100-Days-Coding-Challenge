e212bfa6f94f63da8c9de55bde641fa00b9acaad
AaQib Ali <75785706+aaqibali1@users.noreply.github.com>
2025-01-06 22:38:58 +0500

    Add files via upload

---
+++ Day-01/Day-01.py
@@ -0,0 +1,9 @@
+print("Hello World! \n Hello world \n Hello world \n Hello world")
+
+# this code is working means the file which was made earlier using shell is not encoded correctly, 
+# out task was to upload all the directories in the gitgub and we did it
+# got it Yes i got it thats great so we have to creat ofhter file when needed for pratice
+# yes you have to create one for your practice and you will obviously need it. because all these all currupted
+# well then done let's join dsa session running now ok yah but i did;t completred todays leedode yet
+# it is alright you will complete it later, lets listen some vocabulary, cz we don't know anything hahahaha
+# I got it lets joing and learn
